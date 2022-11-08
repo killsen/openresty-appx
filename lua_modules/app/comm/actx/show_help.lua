098d25be141df1a6536c64ea0727ce68f0a8d171
killsen <killsen@qq.com>
2022-11-08 13:29:24 +0800

    项目帮助页面改用读取相对路径网页

--- lua_modules/app/comm/actx/show_help.lua
+++ lua_modules/app/comm/actx/show_help.lua
@@ -1,13 +1,23 @@
 
+local help_html  -- 网页
+
 local function show_help(app_name)
 
     local app = require "app.comm.appx".new(app_name)
     if not app then return ngx.exit(404) end
 
+    if not help_html then
+        local info = debug.getinfo(1, "S")
+        local path = string.sub(info.source, 2)  -- 去掉开头的@符号
+        local file = io.open(path .. "/../help.html", "rb")
+        if not file then return ngx.exit(404) end
+        help_html = file:read("a")
+        file:close()
+    end
+
     local template  = require "resty.template"
-    local help_html = require "app.comm.actx.help_html"
-    local load_acts = require "app.comm.actx.load_acts"
-    local load_daos = require "app.comm.daox.load_daos"
+    local actx      = require "app.comm.actx"
+    local daox      = require "app.comm.daox"
 
     -- app:clean_up()
 
@@ -18,11 +28,10 @@ local function show_help(app_name)
         app_title   = app.title,
         app_ver     = app.ver,
         help_html   = app.help_html,
-        app_acts    = load_acts(app.name),
-        app_daos    = load_daos(app.name),
+        app_acts    = actx.load_acts(app.name),
+        app_daos    = daox.load_daos(app.name),
     })
 
 end
 
 return show_help
-
