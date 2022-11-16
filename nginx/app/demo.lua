46ba90e06ebf4a73c8f887242851e46fd7c760b8
beck <2223969110@qq.com>
2022-11-16 10:54:47 +0800

    chore: 修正注释

--- nginx/app/demo.lua
+++ nginx/app/demo.lua
@@ -22,7 +22,7 @@ app.help_html  = [[
 --- 帮助文档配置 ----------------------------------
 app.help_config = {
     template = 'help/index.html', -- help 网页模板所在地址：远程地址、本地地址
-    links    = {                  -- 顶部右侧链接配置 { { name, url } }
+    links    = {                  -- 顶部右侧链接配置（数组配置: { text, link }, 对象配置: { text: '', link: '' }）
         { "代码仓库", "https://github.com/killsen/openresty-appx" },
     },
 }
