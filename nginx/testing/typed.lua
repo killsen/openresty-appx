eb92af1fea4dd28f2db83aab35c9f8719352fdfd
killsen <killsen@qq.com>
2023-01-03 16:24:21 +0800

    update typed.lua

--- nginx/testing/typed.lua
+++ nginx/testing/typed.lua
@@ -1,36 +1,46 @@
-
-local _M = { _VERSION = "1.0.1" }
+local _M = { _VERSION = "1.6.1" }
 
 -- 类型声明
 _M.types = {
     TCate = {
-        cate_id     = "string   //类别编码",
+        cate_id     = "number   //类别编码",
         cate_name   = "string   //类别名称",
     },
-    TItem =  {
-        item_id     = "string   //明细编码",
-        item_name   = "string   //明细名称",
+    TDish =  {
+        dish_id     = "number   //菜品编码",
+        dish_name   = "string   //菜品名称",
+        dish_price  = "number   //菜品价格",
     },
-    TCateWithItem = {
-        "@TCate",   -- 继承类别数据
-        item        = "@TItem  //明细数据",
+    TCateWithDishs  = {
+        "@TCate",   -- 继承
+        dish_count  = "number   //菜品数量",
+        dishs       = "@TDish[] //菜品数据",
     }
 }
 
 -- 合并数据
-_M.merge = function(cate, item)
+_M.merge = function(cate, dishs)
 -- @cate    : @TCate            //类别参数
--- @item    : @TItem            //明细参数
--- @return  : @TCateWithItem    //返回类别含明细
+-- @dishs   : @TDish[]          //菜品参数
+-- @return  : @TCateWithDishs   //返回类别含菜品
 
     return {
         cate_id     = cate.cate_id,
         cate_name   = cate.cate_name,
-        not_exist   = "------------",
-        item        = {
-            item_id     = item.item_id,
-            item_name   = item.item_name,
-            not_exist   = "------------",
+        dish_count  = #dishs,
+        not_exist   = "字段未定义",
+        dishs       = {
+            {
+                dish_id     = dishs[1].dish_id,
+                dish_name   = dishs[1].dish_name,
+                dish_price  = dishs[1].dish_price,
+                not_exist_1 = dishs[1].not_exist_1,  -- 字段未定义
+            },{
+                dish_id     = dishs[2].dish_id,
+                dish_name   = dishs[2].dish_name,
+                dish_price  = dishs[2].dish_price,
+                not_exist_2 = dishs[2].not_exist_2,  -- 字段未定义
+            },
         },
     }
 
@@ -39,25 +49,27 @@ end
 -- 调试
 _M._TESTING = function()
 
-    local t = _M.merge({
-        cate_id     = "cate_id",
-        cate_name   = "cate_name",
-        not_exist   = "not_exist",
-    }, {
-        item_id     = "item_id",
-        item_name   = "item_name",
-        not_exist   = "not_exist",
-    })
+    -- @dishs : @TDish[]
+    local dishs = {
+        { dish_id = 1011, dish_name = "牛肉炒芥兰", dish_price = 100 },
+        { dish_id = 1012, dish_name = "红烧狮子头", dish_price = 200 },
+    }
 
-    ngx.say ""
-    ngx.say(t.cate_id)
-    ngx.say(t.cate_name)
-    ngx.say(t.not_exist)
-    ngx.say ""
-    ngx.say(t.item.item_id)
-    ngx.say(t.item.item_name)
-    ngx.say(t.item.not_exist)
-    ngx.say ""
+    local c = _M.merge({ cate_id = 10, cate_name = "热菜" }, dishs)
+
+        ngx.say("cate_id    : ", c.cate_id)
+        ngx.say("cate_name  : ", c.cate_name)
+        ngx.say("dish_count : ", c.dish_count)
+        ngx.say("not_exist  : ", c.not_exist)  -- 字段未定义
+
+    for i, d in ipairs(c.dishs) do
+        ngx.say ""
+        ngx.say("-------- [ ", i, " ] --------")
+        ngx.say ""
+        ngx.say("dish_id    : ", d.dish_id)
+        ngx.say("dish_name  : ", d.dish_name)
+        ngx.say("dish_price : ", d.dish_price)
+    end
 
 end
 
