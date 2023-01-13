55066778aa07e95de72357b2666a055b646f18f6
killsen <killsen@qq.com>
2023-01-13 18:20:13 +0800

    使用 str:gsub 代替 string.gsub

--- nginx/testing/config.lua
+++ nginx/testing/config.lua
@@ -18,9 +18,9 @@ ngx.say "------------------------------------------"
 ngx.say ""
 
 local conf = ngx.config.nginx_configure()
-conf = string.gsub(conf, "--prefix", "\n--prefix")
-conf = string.gsub(conf, "--with-", "\n--with-")
-conf = string.gsub(conf, "--add-", "\n--add-")
+            :gsub("--prefix", "\n--prefix")
+            :gsub("--with-", "\n--with-")
+            :gsub("--add-", "\n--add-")
 ngx.say ( "ngx.config.nginx_configure()")
 ngx.say ( conf )
 ngx.say ""
