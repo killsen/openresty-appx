2fe278c643bd715ade3b5b91ce2ab4c2557dc8eb
killsen <killsen@qq.com>
2023-03-28 15:23:16 +0800

    完善 timer 库参数类型声明

--- lua_modules/app/lib/timer.lua
+++ lua_modules/app/lib/timer.lua
@@ -1,26 +1,28 @@
---[[
-_load "%timer"                      -> timer
-timer.at   (delay, callback, ...)   -> ok, err  -- 延时执行一次
-timer.every(delay, callback, ...)   -> ok, err  -- 定时执行多次
-timer.running_count()               -> count    -- 返回正在执行的定时器数量
-timer.pending_count()               -> count    -- 返回等待执行的定时器数量
---]]
 
 local timer_at      = ngx.timer.at
 local timer_every   = ngx.timer.every
 local pcall         = pcall
 
--- 定时器文档：
--- https://github.com/iresty/nginx-lua-module-zh-wiki/blob/master/README.md#ngxtimerat
+local __ = { _VERSION = "v23.03.28" }
+
+__._README = [[
+[OpenResty定时器](https://github.com/iresty/nginx-lua-module-zh-wiki/blob/master/README.md#ngxtimerat)
+]]
+
+__.running_count = ngx.timer.running_count -- 返回正在执行的定时器数量
+__.pending_count = ngx.timer.pending_count -- 返回等待执行的定时器数量
 
 -- 延时执行一次（单位秒）
-local function _at(delay, callback, ...)
+__.at = function(delay, callback, ...)
+-- @delay       : number
+-- @callback    : function*
+-- @return      : hdl?: number, err?: string
 
     local app_name   = ngx.ctx.app_name   -- 原环境
-    local _load      = _load
+    local _unload    = _load
     local is_running = false
 
-    local ok, err = timer_at(delay, function(premature, ...)
+    local hdl, err = timer_at(delay, function(premature, ...)
 
         if premature then return end    -- 定时器已过期
 
@@ -32,28 +34,32 @@ local function _at(delay, callback, ...)
             local  ok, err = pcall(callback, ...)
             if not ok then ngx.log(ngx.ERR, "timer.at callback error: \n", err) end
 
-            _load() -- 清理相关资源（调用 app.unload）
+            -- 清理相关资源（调用 app.unload）
+            if app_name and type(_unload) == "function" then pcall(_unload) end
 
             is_running = false
 
     end, ...)
 
-    if not ok then
+    if not hdl then
         ngx.log(ngx.ERR, "failed to create timer: ", err)
     end
 
-    return ok, err
+    return hdl, err
 
 end
 
 -- 定时执行多次（单位秒）
-local function _every(delay, callback, ...)
+__.every = function(delay, callback, ...)
+-- @delay       : number
+-- @callback    : function*
+-- @return      : hdl?: number, err?: string
 
     local app_name   = ngx.ctx.app_name   -- 原环境
-    local _load      = _load
+    local _unload    = _load
     local is_running = false
 
-    local ok, err = timer_every(delay, function(premature, ...)
+    local hdl, err = timer_every(delay, function(premature, ...)
 
         if premature then return end    -- 定时器已过期
 
@@ -65,23 +71,19 @@ local function _every(delay, callback, ...)
             local  ok, err = pcall(callback, ...)
             if not ok then ngx.log(ngx.ERR, "timer.every callback error: \n", err) end
 
-            _load() -- 清理相关资源（调用 app.unload）
+            -- 清理相关资源（调用 app.unload）
+            if app_name and type(_unload) == "function" then pcall(_unload) end
 
             is_running = false
 
     end, ...)
 
-    if not ok then
+    if not hdl then
         ngx.log(ngx.ERR, "failed to create timer: ", err)
     end
 
-    return ok, err
+    return hdl, err
 
 end
 
-return {
-        at              = _at
-    ,   every           = _every
-    ,   running_count   = ngx.timer.running_count -- 返回正在执行的定时器数量
-    ,   pending_count   = ngx.timer.pending_count -- 返回等待执行的定时器数量
-}
+return __
